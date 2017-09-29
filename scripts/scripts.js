9e4bd3e28820082dba71d987d7828c0e83647393
KimAbliter <kimabliters@gmail.com>
2017-09-29 21:06:50 +0800

    [add] new styles

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,8 +16,12 @@ $(document).ready(function(){
 
 		scrollLink.each(function() {
 
-			var sectionOffset = $(this.hash).offset().top
+			var sectionOffset = $(this.hash).offset().top -20;
 
+			if (sectionOffset <= scrollbarLocation) {
+				$(this).parent()addClass('active');
+				$(this).parent().siblings().removeClass('active');
+			}
 		})
 	})
 }})
